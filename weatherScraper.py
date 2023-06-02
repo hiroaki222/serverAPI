3247357ad2a24f411b66f09ddf8ab336ce38dbfa
hiroaki222 <hiroaki.takahara.222@gmail.com>
2023-06-02 16:01:56 +0900

    commit

--- weatherScraper.py
+++ weatherScraper.py
@@ -17,12 +17,17 @@ cur.execute('SELECT location FROM profiles WHERE id = ?', (id,))
 
 driver.get(cur.fetchall()[0][0])
 
-element = driver.find_element(By.XPATH, '//*[@id="yjw_pinpoint_today"]/table')
+element = driver.find_element(By.XPATH, '//*[@id="yjw_pinpoint_today"]/table/tbody')
 trs = element.find_elements(By.TAG_NAME, 'tr')
 
+c = [0, 0]
 for i in trs:
     tds = i.find_elements(By.TAG_NAME, 'td')
+    c[0] += 1
     for j in tds:
-        j = j.find_element(By.TAG_NAME, 'img')
-        print(j.get_attribute('alt'))
+        j = j.find_element(By.XPATH, '//td//small//font')
+        print(j.text)
+        c[1] += 1
+    print(c)
 #d = {3 * i - 3 : '' for i in range(1, 9)}
+
